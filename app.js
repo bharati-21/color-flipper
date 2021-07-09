e5cdb180d140dad6cc75879f7a1356a0dabae89c
Bharati Subramanian <bharatisharada@gmail.com>
2021-07-10 00:04:10 +0530

    feat: change bg on button click

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-const btnChangeColor = document.querySelector('.btnChangeColor');
+const btnChangeColor = document.querySelector('.btn-change-color');
 const colorCode = document.querySelector('.color-code');
 const simple = document.querySelector('.simple');
 const hex = document.querySelector('.hex');
+
+let generateHexColors = false;
+
+let currentBgColor = '#FFC2C3';
+colorCode.textContent = currentBgColor;
+
+btnChangeColor.addEventListener('click', changeBgColor);
+
+
+function changeBgColor(e) {
+    e.preventDefault();
+    
+    document.body.style.backgroundColor = currentBgColor;    ;
+}
+
