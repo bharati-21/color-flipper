6a5482b09e2306e8d5cf0c8fecc69b3b7f91f9c4
Bharati Subramanian <bharatisharada@gmail.com>
2021-07-09 23:57:36 +0530

    chore: declared variables

---
+++ app.js
@@ -0,0 +1,4 @@
+const btnChangeColor = document.querySelector('.btnChangeColor');
+const colorCode = document.querySelector('.color-code');
+const simple = document.querySelector('.simple');
+const hex = document.querySelector('.hex');
